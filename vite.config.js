f0ace8bcb50248ea83c5f784ad4a35f69402ebdf
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-05-25 16:34:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/boke/',
+  base: '/boke/',   // 必须与 GitHub 仓库名一致，注意首尾斜杠
   server: {
     port: 3000
   }
