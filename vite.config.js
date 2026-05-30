c54ddf435690c7b3851ad8f66501b55342ea837b
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-05-30 10:29:58 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: './',   // 使用相对路径，适配所有 GitHub Pages 仓库
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
